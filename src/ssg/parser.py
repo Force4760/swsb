28dda0c0eae81924b6ed2cca49ae80f6bbfe9a22
Force4760 <gforce4760@gmail.com>
2021-03-15 14:18:19 +0000

    new build

--- src/ssg/parser.py
+++ src/ssg/parser.py
@@ -5,8 +5,8 @@ import meta
 def parser(md, file):
     g = meta.get_meta(file)
     md = re.sub("Title\: (.)+", "", md)
-    md = re.sub("Authore\: (.)+", "", md)
+    md = re.sub("Author\: (.)+", "", md)
     md = re.sub("Date\: (.)+", "", md)
     md = re.sub("Overview\: (.)+", "", md)
     md = re.sub("(Img|Image)\: (.)+", "", md)
-    return [g,markdown.markdown(md, extensions=['extra', 'admonition', 'codehilite','nl2br','wikilinks', 'fenced_code']).replace("\n", "<br/>")]
+    return [g,markdown.markdown(md, extensions=['extra', 'admonition', 'codehilite','nl2br','wikilinks', 'fenced_code'])]
